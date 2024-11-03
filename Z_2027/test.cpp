2b8c886f47189afe725a6e3144a07ffdb16379e9
Shaurya <shaurya.deoria@gmail.com>
2024-11-03 15:48:43 +0530

    Refactor code formatting and fix bugs in bfs_dfs.cpp, pair.cpp, pq.cpp, lower_upper_bound.cpp, iterators.cpp, 542_01Mat.cpp, flood_fill.cpp, map.cpp, 547_23ms.cpp, disjoint_set_or_unionfind.cpp, lambda.cpp Update input and output files Add new file Number_theory/03_bitmasking.cpp Add new file Z_2027/test.cpp Update .vscode/settings.json Add new file Number_theory/02.cpp Add new file Number_theory/01_starts.txt Add new file Z_2024/q1_profitable.cpp Add new file Z_2033/q1.cpp Add new file Z_2027/q2.cpp Add new file Z_2033/q2.cpp Add new file Z_2027/d1.cpp Add new file Number_theory/00_modulo.cpp Add new file Z_2033/q4.cpp Add new file Z_2024/q2_noofclicks.cpp

---
+++ Z_2027/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define debug(x) cout << #x << " = " << x << endl;
+const long long INF = 1e18;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
